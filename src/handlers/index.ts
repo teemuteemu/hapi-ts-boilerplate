84f6bb312c969e12a055b58305d804b264a73db5
Teemu Kallio <teemu.kallio@dka.io>
2020-02-07 14:33:45 +0100

    Better scripts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,7 +2,7 @@ import { Request, ResponseToolkit } from "@hapi/hapi";
 
 const handlers = {
   index: (request: Request, h: ResponseToolkit) => {
-    return { result: 'yo' };
+    return { result: 'foo' };
   }
 };
 
